4285ec6eaf8914c2969738909b501eb71742485c
pranavkaul <pkaul7@gmail.com>
2020-07-03 17:58:31 +0530

    update

---
+++ Course-3-Using Python to Access Web Data/Assignment3- Beautiful Soup.py
@@ -0,0 +1,29 @@
+"""Scraping Numbers from HTML using BeautifulSoup In this assignment you will write a Python program similar to http://www.py4e.com/code3/urllink2.py. The program will use urllib to read the HTML from the data files below, and parse the data, extracting numbers and compute the sum of the numbers in the file.
+
+We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
+
+Sample data: http://py4e-data.dr-chuck.net/comments_42.html (Sum=2553)
+Actual data: http://py4e-data.dr-chuck.net/comments_690526.html (Sum ends with 38)
+You do not need to save these files to your folder since your program will read the data directly from the URL. Note: Each student will have a distinct data url for the assignment - so only use your own data url for analysis."""
+
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+
+tags = soup('span')
+sum = 0
+for tag in tags:
+    total = int(tag.text)
+    sum = sum + total
+    continue
+print(sum)
